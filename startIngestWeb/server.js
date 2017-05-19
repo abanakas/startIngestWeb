44e13730eaef955ac96c458d6b7177effde275d9
Alex Banakas <abanakas@gmail.com>
2017-05-19 15:31:15 -0400

    learning stuff

--- startIngestWeb/server.js
+++ startIngestWeb/server.js
@@ -7,16 +7,30 @@
 //    res.end('Hello World\n');
 //}).listen(port);
 
-var express = require('express');
-var app = express();
-var path = require('path');
+var http = require('http');
+//var express = require('express');
+//var app = express();
+//var path = require('path');
 
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
-});
+http.createServer(function (request, response) {
+    //Send the HTTP header
+    //HTTP Status: 200 : OK
+    //Content Type: text/plain
+    response.writeHead(200, { 'Content-Type': 'text/plain' });
 
-app.get('/start.html', function (req, res) {
-    res.sendFile(path.join(__dirname + '/start.html'));
-});
+    //Send the response body as "Hello World"
+    response.end('Hello World\n');
+}).listen(1337);
 
-app.listen(1337);
+//Console will print the message
+console.log('Server running at http://127.0.0.1:1337/');
+
+//app.get('/', function (req, res) {
+//    res.sendFile(path.join(__dirname + '/index.html'));
+//});
+
+//app.get('/start.html', function (req, res) {
+//    res.sendFile(path.join(__dirname + '/start.html'));
+//});
+
+//app.listen(1337);
